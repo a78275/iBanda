3741e8d823ee44b1dbf7e1a5e3cbc835e1e18419
a78275 <a78275@alunos.uminho.com>
2018-11-27 11:06:33 +0000

    erro corrigido

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-//var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apisRouter = require('./routes/apis');
-
-var app = express();
-
-//Base de Dados
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 var mongoose = require('mongoose')
-mongoose
-  .connect('mongodb://127.0.0.1:27017/amd', {useNewUrlParser: true})
+
+var uuid = require('uuid/v4')
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
+var axios = require('axios')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+
+//Definição das rotas
+var loginRouter = require('./routes/login')
+var usersRouter = require('./routes/users')
+var apiRouter = require('./routes/api/index')
+
+var app = express()
+
+//Ligação à BD
+mongoose.connect('mongodb://127.0.0.1:27017/amd', {useNewUrlParser: true})
   .then(()=> console.log('Mongo Status: ' + mongoose.connection.readyState))
-  .catch(()=> console.log('Erro na conexão com o Mongo.'))
+  .catch(()=> console.log('Erro na conexão ao Mongo.'))
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/api', apisRouter);
+//Definição dos caminhos das rotas
+app.use('/', loginRouter)
+app.use('/api', apiRouter)
+app.use('/user', usersRouter)
 
-// catch 404 and forward to error handler
+//Apanhar erro 404 e tratar erro
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404))
 });
 
-// error handler
+//Tratar erros
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  //Carregar página de erro
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
